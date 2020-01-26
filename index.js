abb608400285d26425561d2fe99054370a625b7d
susankoju <aksusankoju@gmail.com>
2020-01-26 10:06:04 +0545

    log

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({
 const clients = require('./configs/clientsStats');
 
 app.use(function (req, res, next) {
+    console.log(req);
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
