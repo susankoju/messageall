f7980a6f1d541ce09da6a949625e975f43555930
susankoju <aksusankoju@gmail.com>
2020-01-27 07:40:52 +0545

    access control origin

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use(cors()); //allow everything
 //     origin: 'http://localhost:3000',
 //     credentials: true
 // })); 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
