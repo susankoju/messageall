ab896a1d5d018893236eacc0e757fe3633d75b80
susankoju <aksusankoju@gmail.com>
2020-01-27 07:47:19 +0545

    cors

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors()); //allow everything
 //     credentials: true
 // })); 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://susankoju.github.io"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
